8d15ab4a827c214b38047f771d20053d961f1183
Scott McDonald <scott.mcdonald@marketdial.com>
2022-02-01 08:28:33 -0700

    First iteration of bad code challenge complete.

--- test_bad_code.py
+++ test_bad_code.py
@@ -4,25 +4,22 @@ import os
 import unittest
 import copy
 
-import challenges
+import bad_code
 
 CLIENT_NAME = "tc_topps"
-
-SKU = '48052'
-
+SKU = "48052"
 EXPECT = [{"name": SKU, "price": "15.99", "quantity_sold": 54}]
 DB_CLIENT = {"client_collection": {CLIENT_NAME: {"items_sold": EXPECT}}}
 REPORT_FILENAME = "/tmp/TC_Finance_Report.csv"
 
 
 class TestBadCodeChallenge(unittest.TestCase):
-
     def test_send_report(self):
 
         if os.path.exists(REPORT_FILENAME):
             os.remove(REPORT_FILENAME)
 
-        self.assertTrue(challenges.send_report(copy.deepcopy(DB_CLIENT), rejects=False))
+        self.assertTrue(bad_code.send_report(copy.deepcopy(DB_CLIENT), rejects=False))
 
         with open(REPORT_FILENAME, newline="") as csvfile:
             reader = csv.DictReader(csvfile)
@@ -34,3 +31,7 @@ class TestBadCodeChallenge(unittest.TestCase):
 
         if os.path.exists(REPORT_FILENAME):
             os.remove(REPORT_FILENAME)
+
+
+if __name__ == "__main__":
+    unittest.main()
