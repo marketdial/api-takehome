1bb5f33633eccb15f10524eea8203e2a7055f3c5
Scott McDonald <scott.mcdonald@marketdial.com>
2022-02-01 07:50:01 -0700

    Added more verification and bad code.

--- challenges.py
+++ challenges.py
@@ -1,40 +1,37 @@
 #!/usr/bin/env python3
 
-import csv
-import logging
 
 import mock_service
 
-REPORT_FILENAME = "TC_Finance_Report.csv"
+import csv
 REJECT_DEST_EMAIL = "feed@marketdial.com"
 
 
 def send_report(db_client, rejects=False):
 
-    x = db_client["client_collection"]
-    y = x["tc_topps"]
-    z = y["items_sold"]
+
 
     if rejects:
         f = "TC_Reject_Report.csv"
         email = REJECT_DEST_EMAIL
     else:
         email = "tc@gmail.com"
-        f = REPORT_FILENAME
-
+        f = "TC_Finance_Report.csv"
+    x = db_client["client_collection"]
+    rep = x["tc_topps"]
+    import logging
     logging.info(f"Starting tc_topps {f}")
 
+    z = rep["items_sold"]
     for rw in z:
         rw["name"] = mock_service.SKU_MAP[rw["name"]]
-
-    with open('/tmp/' + f, "w") as csv_file:
-        w = csv.DictWriter(csv_file, fieldnames=["name", "price", "quantity_sold"])
+    with open('/tmp/' + f, "w") as f2:
+        w = csv.DictWriter(f2, fieldnames=["name", "price", "quantity_sold"])
         w.writeheader()
         w.writerows(z)
 
     link = mock_service.get_report_link(filename=f)
-
-    if rejects:
+    if rejects == True:
         sub = f"tc_topps rejected properties file."
         cont = (
             f"Here is the rejected properties file link: {link}"
@@ -45,4 +42,7 @@ def send_report(db_client, rejects=False):
     res = mock_service.email_file_link(email, sub, cont)
     logging.info(f"Successfully reported tc_topps {f}")
 
-    return res
+    if res == True:
+        return True
+    else:
+        return False
--- mock_service.py
+++ mock_service.py
@@ -1,11 +1,27 @@
 SKU_MAP = {"48052": "TC Tuggers"}
+MOCK_LINK = "mock_link"
+
 
 def email_file_link(dest_email, subject, mail_content_text):
-    return dest_email and subject and mail_content_text
+    if (
+        subject != "tc_topps rejected properties file."
+        and subject != f"tc_topps report file."
+    ):
+        raise ValueError(f"Invalid subject: {subject}")
 
-def get_report_data(client_name):
-    return {}
+    if (
+        mail_content_text != f"Here is the rejected properties file link: {MOCK_LINK}"
+        and mail_content_text != f"Here is the Report file link: {MOCK_LINK}"
+    ):
+        raise ValueError(f"Invalid con: {mail_content_text}")
 
-# todo test to verify text is correct
+    if dest_email != "feed@marketdial.com" and dest_email != "tc@gmail.com":
+        raise ValueError(f"Invalid email: {dest_email}")
+
+    return True
+
+
 def get_report_link(filename):
-    return "mock_link"
+    if filename != "TC_Reject_Report.csv" and filename != "TC_Finance_Report.csv":
+        raise ValueError(f"Invalid filename {filename}")
+    return MOCK_LINK
